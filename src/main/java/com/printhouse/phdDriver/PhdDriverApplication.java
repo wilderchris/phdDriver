f421beb45323e2b2343d024db49cd500a446c3e4
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-03-10 10:42:34 -0500

    Initial commit

---
+++ src/main/java/com/printhouse/phdDriver/PhdDriverApplication.java
@@ -0,0 +1,13 @@
+package com.printhouse.phdDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhdDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhdDriverApplication.class, args);
+	}
+
+}
